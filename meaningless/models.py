dc498b0a2f84fc2cec17a69355c83f1c0b349dde
Pasha <hurinovichpasha@gmail.com>
2020-09-08 16:18:29 +0300

    registration form

--- meaningless/models.py
+++ meaningless/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    birthday = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
--- meaningless/views.py
+++ meaningless/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def home_view(request):
+    context = {}
+    return render(request, 'meaningless/index.html', context)
+
+
+def login_view(request):
+    context = {}
+    return render(request, 'meaningless/login.html', context)
+
+
+def register_view(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'meaningless/register.html', context)
+
